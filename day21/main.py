3d1ebe11c6a567f0f439f5b6fe6dc46f46fe1514
Joseph Buell <jlrbuellv@gmail.com>
2024-12-31 17:12:03 -0600

    Day 21 Complete

--- day21/main.py
+++ day21/main.py
@@ -1,6 +1,7 @@
 from math import inf
+from functools import cache
 
-data = open("input.txt").read().split("\n")
+data = [l for l in open("input.txt").read().split("\n") if l]
 
 grid1 = {
     "7": (0, 0),
@@ -56,11 +57,14 @@ def f(grid, x, y, sequence):
 
 def expand(start, dest):
     best = inf
+    a, b, c = "", "", ""
     for r1 in f(grid1, *start, dest):
         for r2 in f(grid2, *grid2["A"], r1):
             for r3 in f(grid2, *grid2["A"], r2):
                 if len(r3) < best:
                     best = len(r3)
+                    a, b, c = r1, r2, r3
+    # print(a, b, c)
     return best
 
 
@@ -72,21 +76,42 @@ def part1(data):
         for c in line:
             complexity += expand(start, c)
             start = grid1[c]
-
         score += int(line[:3]) * complexity
     return score
 
+def paths(grid):
+    paths = {}
+    for source, (x1, y1) in grid.items():
+        for dest, (x2, y2) in grid.items():
+            path = "<" * (x1 - x2) + "v" * (y2 - y1) + "^" * (y1 - y2)  + ">" * (x2 - x1)
+            if ((x1, y2) not in grid.values()) or ((x2, y1) not in grid.values()):
+                path = path[::-1]
+            paths[(source, dest)] = path + "A"
+    return paths
+
+
 def part2(data):
+    p1s = paths(grid1)
+    p2s = paths(grid2)
+
+    @cache
+    def size(seq, i, first=False):
+        if i == 0:
+            return len(seq)
+        source = "A"
+        s = 0
+        paths = p1s if first else p2s
+        for dest in seq:
+            s += size(paths[(source, dest)], i-1)
+            source = dest
+        return s
+    
     score = 0
     for line in data[:]:
-        complexity = 0
-        start = grid1["A"]
-        for c in line:
-            complexity += expand(start, c)
-            start = grid1[c]
+        complexity = size(line, 26, first=True)
 
         score += int(line[:3]) * complexity
     return score
 
 print(part1(data))
-# print(part2(data))
+print(part2(data))
